c2d0d0da2085aa483b111ff0f63c9a5be523ca0e
Meherab <jubayet0106@gmail.com>
2022-06-21 18:45:25 +0600

    final push

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { BlogContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+import './Details.css'
 
 const Details = () => {
   const [blogs] = useContext(BlogContext);
+  const navigate = useNavigate();
 
   let { Key } = useParams();
   const blog = blogs.find((blog) => blog.productKey == Key);
   console.log(blog);
   return (
     <div>
+        <button className='back-button' onClick={() => navigate(-1)}>GO BACK</button>
       <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex-col lg:flex-row-reverse">
           <img
@@ -19,9 +23,7 @@ const Details = () => {
           />
           <div>
             <h1 class="text-5xl font-bold">{blog.name}</h1>
-            <p class="py-6">
-                {blog.description}
-            </p>
+            <p class="py-6">{blog.description}</p>
             <button class="btn btn-primary">{blog.price}</button>
           </div>
         </div>
